3b65b112dcc6741110cdaf383bc109006b4ad52f
Brian David <beejunk@gmail.com>
2019-11-19 23:28:14 -0600

    Remove web_modules.

--- web_modules/htm/preact.js
+++
@@ -1,7 +0,0 @@
-import { render as E, createElement as h } from '../preact.js';
-export { Component, createElement as h } from '../preact.js';
-import e from '../htm.js';
-
-function m(r,t){E(r,t,t.firstElementChild);}var n=e.bind(h);
-
-export { n as html, m as render };
